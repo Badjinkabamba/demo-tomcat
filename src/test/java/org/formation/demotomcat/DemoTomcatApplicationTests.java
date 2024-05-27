978ea65f6b129254005563edf8006c87248c0fa5
bamba <badjinkabamba@gmail.com>
2024-05-27 15:35:57 +0000

    initiale commit

---
+++ src/test/java/org/formation/demotomcat/DemoTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.demotomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
